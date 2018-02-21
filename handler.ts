6cefd40b711087441d7690107339988b9cc706eb
paullessing <paul@paullessing.com>
2018-02-21 20:37:48 +0000

    Initial commit

---
+++ handler.ts
@@ -0,0 +1,13 @@
+import { APIGatewayEvent, Callback, Context } from 'aws-lambda';
+
+export const hello = (event: APIGatewayEvent, context: Context, callback: Callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello World',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
