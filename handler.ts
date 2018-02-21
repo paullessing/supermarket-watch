9920de18327b2719536ab5374dbe8c7fa90042e8
paullessing <paul@paullessing.com>
2018-02-21 22:04:42 +0000

    Add Sainsbury's Scraper

--- handler.ts
+++ handler.ts
@@ -1,22 +1,33 @@
 import { APIGatewayEvent, Callback, Context } from 'aws-lambda';
 import * as request from 'request-promise';
+import * as cheerio from 'cheerio';
 
 export const getProduct = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
   const id = (event.queryStringParameters || {}).id || '';
   console.log('Request ID:', id);
-  const match = id.match(/^(\w+)\:([\w-]+)$/);
+  const match = id.match(/^(\w+)\:(.+)$/);
   if (!match) {
     return callback(null, { statusCode: 400, body: '"Missing or invalid ID"' });
   }
   switch (match[1]) {
     case 'waitrose':
-      const result = await fetchWaitrose(match[2]);
-      callback(null, { statusCode: 200, body: JSON.stringify(result) });
+      callback(null, handleResult(await fetchWaitrose(match[2])));
+      break;
+    case 'sainsburys':
+      callback(null, handleResult(await fetchSainsburys(match[2])));
       break;
     default:
       return callback(null, { statusCode: 400, body: '"Unsupported supermarket"' });
   }
 };
+
+function handleResult(result: any) {
+  if (!result) {
+    return { statusCode: 404 };
+  } else {
+    return { statusCode: 200, body: JSON.stringify(result) };
+  }
+}
 
 const fetchWaitrose = async (productId: string) => {
   const tokenBody = await request('https://www.waitrose.com/api/authentication-prod/v2/authentication/token');
@@ -32,5 +43,35 @@ const fetchWaitrose = async (productId: string) => {
   console.log('Result:', search);
 
   const searchResult = JSON.parse(search);
-  return searchResult.products[0];
+  if (!searchResult.products.length) {
+    return null;
+  } else {
+    const product = searchResult.products[0];
+    return {
+      name: product.name,
+      price: product.currentSaleUnitPrice.price.amount
+    };
+  }
 };
+
+const fetchSainsburys = async (productLink: string) => {
+  console.log('Requesting:', `https://www.sainsburys.co.uk/shop/gb/groceries/${productLink}`);
+  const search = await request({ url: `https://www.sainsburys.co.uk/shop/gb/groceries/${productLink}`, jar: request.jar()}); // Need to pretend we want cookies, else the server refuses to serve us
+  console.log('Result:', search);
+
+  const $ = cheerio.load(search);
+
+  const hasResult = $('.productContent').length > 0;
+  if (!hasResult) {
+    return null;
+  }
+
+  const price = $('.pricePerMeasure').text().replace(/[^\d.]+/g, '');
+  const name = $('.productTitleDescriptionContainer h1').text();
+
+  return {
+    name, price
+  };
+};
+
+//persil-powder-biological-425kg-%2850-washes%29
