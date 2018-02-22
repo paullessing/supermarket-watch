fc0559fc5fd50f2746fd3b88f7aafb3264bfb3ce
paullessing <paul@paullessing.com>
2018-02-22 21:47:57 +0000

    Add "fetch multiple" abstract method

--- supermarkets/supermarket.ts
+++ supermarkets/supermarket.ts
@@ -1,6 +1,12 @@
 import { Product } from './product';
 
 export abstract class Supermarket {
+
   public async init(): Promise<void> {}
+
   public abstract getProduct(id: string): Promise<Product | null>;
+
+  public async getProducts(ids: string[]): Promise<(Product | null)[]> {
+    return await Promise.all(ids.map(id => this.getProduct(id)));
+  }
 }
