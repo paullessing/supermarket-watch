8a984634053e2dd8f36510758c76f742d2993967
paul.lessing <paul@paullessing.com>
2018-02-25 19:36:05 +0000

    Add dependency on serverless-api-wrapper

--- handler.ts
+++ handler.ts
@@ -1,38 +1,4 @@
-import { APIGatewayEvent, Callback, Context } from 'aws-lambda';
-import { Waitrose } from './supermarkets/waitrose';
-import { Sainsburys } from './supermarkets/sainsburys';
-import { Supermarket } from './supermarkets/supermarket';
+import { aws as wrapper } from 'serverless-api-handlers';
+import * as handlers from './product-handler';
 
-export const getProduct = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
-  const id = (event.queryStringParameters || {}).id || '';
-  console.log('Request ID:', id);
-  const match = id.match(/^(\w+)\:(.+)$/);
-  if (!match) {
-    return callback(null, { statusCode: 400, body: '"Missing or invalid ID"' });
-  }
-  const productId = match[2];
-  switch (match[1]) {
-    case 'waitrose':
-      callback(null, handleResult(await fetch(new Waitrose(), productId)));
-      break;
-    case 'sainsburys':
-      callback(null, handleResult(await fetch(new Sainsburys(), productId)));
-      break;
-    default:
-      return callback(null, { statusCode: 400, body: '"Unsupported supermarket"' });
-  }
-};
-
-function handleResult(result: any) {
-  if (!result) {
-    return { statusCode: 404 };
-  } else {
-    return { statusCode: 200, body: JSON.stringify(result) };
-  }
-}
-
-const fetch = async (supermarket: Supermarket, productId: string) => {
-  await supermarket.init();
-  const product = await supermarket.getProduct(productId);
-  return product || null;
-};
+export const getProduct = wrapper.wrap(handlers.getProduct);
---
+++ product-handler.ts
@@ -0,0 +1,36 @@
+import { Waitrose } from './supermarkets/waitrose';
+import { Sainsburys } from './supermarkets/sainsburys';
+import { Supermarket } from './supermarkets/supermarket';
+import { HandlerRequest, HandlerResponse } from 'serverless-api-handlers';
+
+export const getProduct = async (request: HandlerRequest): Promise<HandlerResponse> => {
+  const id = request.queryParameters.id as string || '';
+  console.log('Request ID:', id);
+  const match = id.match(/^(\w+)\:(.+)$/);
+  if (!match) {
+    return { statusCode: 400, body: 'Missing or invalid ID' };
+  }
+  const productId = match[2];
+  switch (match[1]) {
+    case 'waitrose':
+      return handleResult(await fetch(new Waitrose(), productId));
+    case 'sainsburys':
+      return handleResult(await fetch(new Sainsburys(), productId));
+    default:
+      return { statusCode: 400, body: '"Unsupported supermarket"' };
+  }
+};
+
+function handleResult(result: any) {
+  if (!result) {
+    return { statusCode: 404 };
+  } else {
+    return { statusCode: 200, body: JSON.stringify(result) };
+  }
+}
+
+const fetch = async (supermarket: Supermarket, productId: string) => {
+  await supermarket.init();
+  const product = await supermarket.getProduct(productId);
+  return product || null;
+};
