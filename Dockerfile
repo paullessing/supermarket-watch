12c2c123935d81adb4074f149e340ce81be332c9
paullessing <paul@paullessing.com>
2020-01-14 17:34:19 +0000

    Add Angular frontend
    Squashed commit of the following:
    
    commit 81a49ac87013448cbbec5fe7b1e96af8d1a2dd30
    Author: paullessing <paul@paullessing.com>
    Date:   Tue Jan 14 17:33:39 2020 +0000
    
        Add frontend node modules to volumes
    
    commit 38f911f1636ac5f8532998ad6f39e21ede0cb927
    Author: paullessing <paul@paullessing.com>
    Date:   Tue Jan 14 17:25:09 2020 +0000
    
        Fix docker build process
    
    commit f0948cd8e25f9da0d57c7e48f92fa754ae76fd2c
    Author: paullessing <paul@paullessing.com>
    Date:   Tue Jan 14 16:35:12 2020 +0000
    
        Fix build for docker
    
    commit 80f58e6b713d041c0f8c794707d6a86a5fef9968
    Author: paullessing <paul@paullessing.com>
    Date:   Tue Jan 14 16:25:54 2020 +0000
    
        Set up frontend code being served properly
    
    commit 382b3600f0e9f9d10d8689961067f9c0ee88c236
    Author: paullessing <paul@paullessing.com>
    Date:   Tue Jan 14 16:01:40 2020 +0000
    
        Set up frontend sources

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,13 @@ FROM node:lts-alpine3.11
 WORKDIR /usr/src/app
 
 COPY package.json yarn.lock ./
+COPY frontend/package.json frontend/yarn.lock ./frontend/
 RUN yarn install
 
-COPY tsconfig.json src ./
+COPY tsconfig.json ./
+COPY src ./src
+COPY frontend ./frontend
 
-CMD [ "yarn", "serve:prod" ]
+RUN yarn build
+
+CMD [ "yarn", "start" ]
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,23 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import proxy from 'express-http-proxy';
 import morgan from 'morgan';
+import path from 'path';
 import { config } from './config.service';
-import { supermarketService } from './supermarket.service';
+import { productController } from './controllers/product.controller';
+import { searchController } from './controllers/search.controller';
 
 const app = express();
 
-app.use(morgan(config.environment === 'development' ? 'dev' : 'combined'));
+app.use(morgan('dev'));
 
-app.get('/api/product/:id', async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id || '';
-    if (!id) {
-      return res.status(400).send('Missing required URL parameter "id"');
-    }
-    const item = await supermarketService.getSingleItem(id);
-    if (!item) {
-      return res.status(404).send('Not Found');
-    }
-    return res.status(200).send(JSON.stringify(item));
-  } catch (e) {
-    console.error(e);
-    return res.status(500).send('Internal Server Error');
-  }
-});
+app.use('/api/product', productController);
+app.use('/api/search', searchController);
 
-app.get('/api/search', async (req: Request, res: Response) => {
-  try {
-    const query = req.query.q || '';
-    if (!query) {
-      return res.status(400).send('Missing required query parameter "q"');
-    }
-    const items = await supermarketService.search(query);
-    if (!items) {
-      return res.status(404).send('Not Found');
-    }
-    return res.status(200).send(JSON.stringify({ items }));
-  } catch (e) {
-    console.error(e);
-    return res.status(500).send('Internal Server Error');
-  }
-});
+if (config.environment === 'prod') {
+  app.use(express.static(path.join(__dirname, 'frontend')));
+  app.use((req, res) => res.sendFile(path.join(__dirname, 'frontend/index.html'))); // Send unexpected routes to the root index.html
+} else {
+  app.use(proxy('http://localhost:4200'));
+}
 
 app.listen(config.port, () => console.log(`App running on port ${config.port}`));
