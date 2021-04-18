434ae7a04925a085c194abc48e40839640712131
paullessing <paul@paullessing.com>
2021-04-18 20:31:39 +0100

    WIP Price Controller (drop this)

--- apps/api/src/app/price.controller.ts
+++ apps/api/src/app/price.controller.ts
@@ -1,16 +1,27 @@
-import { BadRequestException, Controller, Get, NotFoundException, Param, Query } from '@nestjs/common';
-import { Product } from '@shoppi/api-interfaces';
-import { SupermarketService } from './supermarkets';
+import { BadRequestException, Controller, Get, NotFoundException, Param } from '@nestjs/common';
 
-@Controller('api/products')
-export class ProductsController {
+export interface Price {
+  cost: number;
+  costPerUnit: number;
+  unitCount: number;
+  unit: string;
+}
+
+export interface ReturnValue {
+  name: string;
+  imageUrl: string;
+  price: Price;
+  defaultPrice: Price;
+}
+
+@Controller('api/price')
+export class PriceController {
 
   constructor(
-    private supermarketService: SupermarketService
   ) {}
 
-  @Get('/:id')
-  public async getById(@Param('id') id: string): Promise<Product> {
+  @Get(':id')
+  public async getById(@Param('id') id: string): Promise<ReturnValue> {
     if (!id) {
       throw new BadRequestException('Missing required URL parameter "id"');
     }
@@ -20,29 +31,5 @@ export class ProductsController {
     }
 
     return item;
-  }
-
-  @Get('/')
-  public async getMultipleById(@Query('ids') idsQuery: string | string[]): Promise<{ items: Product[] }> {
-    if (!idsQuery) {
-      throw new BadRequestException('Missing required URL parameter "ids"');
-    }
-    const ids = Array.isArray(idsQuery) ? idsQuery : idsQuery.split(',');
-
-    if (!ids.length) {
-      throw new BadRequestException('Missing required URL parameter "ids"');
-    }
-
-    const items = await Promise.all(ids.map(async (id): Promise<Product> => {
-      const item = await this.supermarketService.getSingleItem(id);
-      if (!item) {
-        throw new NotFoundException();
-      }
-      return item;
-    }));
-
-    return {
-      items
-    };
   }
 }
