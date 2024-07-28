d7e0f2bb88b428a32545a3e0dbe3c734e82e5020
Paul Lessing <paul@paullessing.com>
2024-07-28 12:26:21 +0100

    fix: Update Tesco parsing for single products (#100)

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-const { spawn } = require('child_process');
+const puppeteer = require('puppeteer');
 const compression = require('compression');
 const express = require('express');
-const app = express();
+
+const port = 3333;
 
 const tescoUrl = 'https://www.tesco.com/groceries/en-GB/';
 const sainsburysUrl = 'https://www.sainsburys.co.uk/groceries-api/gol-services/product/v1/';
@@ -11,7 +12,24 @@ const curlHeaders = [
   'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
   'accept-language: en-GB,en-US;q=0.9,en;q=0.8,de;q=0.7',
 ].reduce((acc, curr) => acc.concat('-H', curr), []);
-const port = 3333;
+
+const app = express();
+/**
+ * @type puppeteer.Browser
+ */
+let browser;
+
+async function loadPage(url) {
+  const page = await browser.newPage();
+  await page.setUserAgent(
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+  );
+  await page.goto(url, { waitUntil: 'networkidle0' });
+
+  const data = await page.content();
+
+  return data;
+}
 
 async function streamFromUrl(url, res) {
   return await new Promise((resolve, reject) => {
@@ -42,9 +60,11 @@ app.get('/tesco/product/:id', async (req, res) => {
       return res.status(400).end();
     }
     console.log(`Fetching ${productId}`);
-    const result = await streamFromUrl(`${tescoUrl}products/${encodeURIComponent(productId)}`, res);
+    const result = await loadPage(`${tescoUrl}products/${encodeURIComponent(productId)}`);
 
-    console.log(`Got ${result} bytes`);
+    res.send(result);
+
+    console.log(`Got ${result.length} bytes`);
     res.end();
   } catch (e) {
     console.log(e);
@@ -90,6 +110,19 @@ app.get('/sainsburys/product', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+console.log('Launching Puppeteer...');
+puppeteer
+  .launch()
+  .then((_browser) => {
+    browser = _browser;
+    console.log('Puppeteer is started');
+
+    console.log('Starting express...');
+    app.listen(port, () => {
+      console.log(`App listening at http://localhost:${port}`);
+    });
+  })
+  .catch((e) => {
+    console.error('An error has occurred:', e);
+    process.exit(1);
+  });
