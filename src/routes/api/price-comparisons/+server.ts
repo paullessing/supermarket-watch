8766cd2bda2b19f0a51555b465641dfc915dca4a
Paul Lessing <paul@paullessing.com>
2025-06-27 10:46:08 +0100

    chore: move DELETE /api/price-comparisons/all to /api/price-comparisons

--- src/routes/api/price-comparisons/+server.ts
+++ src/routes/api/price-comparisons/+server.ts
@@ -77,3 +77,14 @@ export async function POST({ request }): Promise<Response> {
     trackingId: resultId,
   });
 }
+
+export async function DELETE(): Promise<Response> {
+  const productRepo = await $productRepository;
+
+  await productRepo.removeAllComparisons();
+  await productRepo.removeAllHistory();
+
+  return new Response(null, {
+    status: 204,
+  });
+}
